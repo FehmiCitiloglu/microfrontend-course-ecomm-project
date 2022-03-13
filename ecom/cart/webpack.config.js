664bc18a447ed4edbe52645cd872550de16239f7
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-03-13 23:29:27 +0300

    basics of module federation section end

--- ecom/cart/webpack.config.js
+++ ecom/cart/webpack.config.js
@@ -1,21 +1,24 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+
 
 module.exports = {
     mode: 'development',
     devServer: {
-        port: 8080,
+        port: 8082,
     },
     plugins: [
         new ModuleFederationPlugin({
-            name: 'container',
-            remotes: {
-                products: 'products@http://localhost:8081/remoteEntry.js',
-                cart: 'cart@http://localhost:8082/remoteEntry.js'
-            },
+            name: 'cart',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './CartShow': './src/index.js'
+            }
         }),
         new HtmlWebpackPlugin({
-            template: './public/index.html',
+            template: './public/index.html'
         })
     ]
 }
